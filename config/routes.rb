067bfe2cafe701285f105b23b7e6525af9e80919
Joseph Ampadu <josephampadu549@gmail.com>
2022-02-07 02:34:17 +0000

    user sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@
 Rails.application.routes.draw do
   get "/about", to: "about#index"
 
+  get "/sign-up", to: "registrations#new"
+  post "/sign-up", to: "registrations#create"
+
   root to: "main#index"
 end
